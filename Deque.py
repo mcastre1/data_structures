e4bb6ca149bd36505892a0aa10119b72b7c2d781
Miguel Castrejon <miguelcastrejon@mail.weber.edu>
2022-03-20 20:18:48 -0600

    Implementation of Deque

---
+++ Deque.py
@@ -0,0 +1,33 @@
+class Deque:
+    '''
+    Implementation of Deque
+    '''
+    def __init__(self):
+        self.items = []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+
+d = Deque()
+
+d.addFront("Hello")
+d.addRear("World")
+
+print(d.size())
+print(d.removeFront() + " " + d.removeRear())
